820a9d597081ce3facdffe741469b782e43dafb5
lucapcf <119978877+lucapcf@users.noreply.github.com>
2024-03-18 00:14:57 -0300

    initial commit

---
+++ supersena/supersena/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=["POST"])
+def index():
+    if request.method == "POST":
+        if request.ok():
+            
+
+    name = "string"
+    return render_template("index.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ supersena/supersena/static/script.js
@@ -0,0 +1,10 @@
+let name = document.getElementsByName('name').value;
+let cpf = document.getElementsByName('cpf').value;
+let numbers = [];
+numbers.push(document.getElementsByName('number')[0].value);
+numbers.push(document.getElementsByName('number')[1].value);
+numbers.push(document.getElementsByName('number')[2].value);
+numbers.push(document.getElementsByName('number')[3].value);
+numbers.push(document.getElementsByName('number')[4].value);
+
+console.log(numbers)
