80b2e5dddbefddcee5c7f86d32fa61129cf58a80
lucapcf <119978877+lucapcf@users.noreply.github.com>
2024-03-20 09:39:01 -0300

    funtional routes and database

--- supersena/__init__.py
+++ supersena/__init__.py
@@ -1,18 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+# from app import create_session, add_bet
+from app import session, add_bet
 
 app = Flask(__name__)
 
-@app.route('/aposta', methods=["POST"])
-def aposta():
+@app.route('/')
+@app.route('/index')
+def index():
+    print(request.method)
+    print(request.form.get('start'))
+    # if request.method == "GET" and request.form.get('start'):
+    #     return redirect("aposta")
+    return render_template("index.html")
+
+@app.route('/aposta', methods=["POST", "GET"])
+# @app.route('/')
+def aposta():    
     if request.method == "POST":
-        if request.ok():
-            print("shit")
+        # if request.ok():
+        name = request.form.get('name')
+        cpf = request.form.get('cpf')
+        n1 = request.form.get('n1')
+        n2 = request.form.get('n2')
+        n3 = request.form.get('n3')
+        n4 = request.form.get('n4')
+        n5 = request.form.get('n5')
 
-    name = "string"
-    return render_template("index.html", name=name)
+        # session = add_bet(session, name, cpf, n1, n2, n3, n4, n5)
+        add_bet(session, name, cpf, n1, n2, n3, n4, n5)
 
-@app.route('/premiacao')
+    return render_template("aposta.html")
+
+@app.route('/premiacao', methods=["GET"])
 def premiacao():
+    return render_template("premiacao.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- supersena/old_code/app_antigo.py
+++ supersena/old_code/app_antigo.py
@@ -2,13 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Aposta
 
-
-engine = create_engine("sqlite:///mydb.db", echo=True)
-Base.metadata.create_all(bind=engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+def create_session():
+    engine = create_engine("sqlite:///mydb.db", echo=True)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
 
 def add_bet(session, name, cpf, n1, n2, n3, n4, n5):
+    session = create_session()
     aposta = Aposta(name, cpf, n1, n2, n3, n4, n5)
     try:
         session.add(aposta)
@@ -20,9 +21,8 @@ def add_bet(session, name, cpf, n1, n2, n3, n4, n5):
     finally:
         session.close()
         return session
-    
-def main():
-    pass
 
 if __name__ == "__main__":
-    main()
+    session = create_session()
+    session = add_bet(session, "luca", "032.607.973-11", 2, 34, 22, 12, 5)
+    session = add_bet(session, "joao", "032.607.973-11", 2, 34, 22, 12, 5)
